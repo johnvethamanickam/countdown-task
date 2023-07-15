ec028744c4507549f55ae4bca413e4bc63b675af
John Vethamanickam <125032599+johnvethamanickam@users.noreply.github.com>
2023-07-15 11:59:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+const num = document.querySelector(".count");
+
+setTimeout(function () {
+  num.innerText = "10";
+
+   setTimeout(function () {
+     num.innerText = "9";
+
+       setTimeout(function () {
+         num.innerText = "8";
+
+          setTimeout(function () {
+            num.innerText = "7";
+
+             setTimeout(function () {
+               num.innerText = "6";
+
+                setTimeout(function () {
+                  num.innerText = "5";
+
+                   setTimeout(function () {
+                     num.innerText = "4";
+
+                      setTimeout(function () {
+                        num.innerText = "3";
+
+                         setTimeout(function () {
+                           num.innerText = "2";
+ 
+                            setTimeout(function () {
+                              num.innerText = "1";
+   
+                               setTimeout(function () {
+                                 num.innerText = "Happy Independence Day!";
+                               }, 1000);
+
+                            }, 1000);
+
+                         }, 1000);
+
+                       }, 1000);
+
+                   }, 1000);
+
+                }, 1000);
+
+             }, 1000);
+
+          }, 1000);
+
+       }, 1000);
+
+    }, 1000);
+
+}, 1000);
+
+
+
+setTimeout(function(){
+    document.querySelector('.image').style.display = "block"
+},11000);
+
